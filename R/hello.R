aa281c94b4eb2144e65182cedac338d4ad4c6eeb
AlissonRP <alirpereira887@gmail.com>
2022-12-19 15:24:17 -0300

    sigma mle

--- R/hello.R
+++ R/hello.R
@@ -13,22 +13,23 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal") {
     unlist()
   X <- data[, definir_parametros(as.character(mu_formula[[3]]), data = data)$covar]
   if (is.null(var_formula)) {
-    sigma <- rep(rpois(1, lambda = 15), length(y))
+    sigma <- rep(rpois(1, lambda = rpois(1, lambda = 3)), length(y))
     cov_sigma <- matrix(diag(sigma), ncol = length(sigma))
   } else {
     sigma <- var_formula(X)
     cov_sigma <- matrix(diag(sigma), ncol = length(y))
   }
   parameters <- definir_parametros(as.character(mu_formula[[3]]), data = data)$parametros
-  par <- runif(length(parameters))
 
+  par <- runif(length(parameters))
+  if (is.null(var_formula)){
+    par <- c(runif(length(parameters)), unique(sigma))
+  }
 
   model <- c()
 
 
   logvero <- function(par) {
-    y <- dados[, 1]
-    X <- dados[, -1]
     for (i in 1:length(parameters)) {
       assign(parameters[i], par[i])
     }
@@ -54,8 +55,14 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal") {
 
   model$coefficients <- coefficients
   model$names <- parameters
+  if (is.null(var_formula)){
+    model$names <- c(parameters, 'sigma')
+  }
+
+
   model_presentation <- data.frame(model$names, model$coefficients)
   names(model_presentation) <- c("Parameters", "Estimates")
+  detach(data)
   return(model_presentation)
 }
 
@@ -67,5 +74,16 @@ y <- 2 / exp(5*X$x1) + log(3*X$x2)
 dados <- data.frame(y, X)
 
 generalreg(dados, mu_formula = y ~ beta0 / exp(beta1*x1) + log(beta2*x2), dist='normal')
+
+
+generalreg(mtcars, mu_formula = mpg ~ alfa + 1 / (beta*disp), dist='normal')
+
+
+
+
+
+
+
+
 
 
