740c8b36a37eade979362b7cbf2420587f78f5a0
AlissonRP <alirpereira887@gmail.com>
2022-12-20 15:01:44 -0300

    var_formula structure (not 100% working?)

--- R/hello.R
+++ R/hello.R
@@ -12,20 +12,23 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal") {
   y <- data[, 1] %>%
     unlist()
   X <- data[, definir_parametros(as.character(mu_formula[[3]]), data = data)$covar]
+  parameters <- definir_parametros(as.character(mu_formula[[3]]), data = data)$parametros
   if (is.null(var_formula)) {
     sigma <- rep(rpois(1, lambda = rpois(1, lambda = 3)), length(y))
     cov_sigma <- matrix(diag(sigma), ncol = length(sigma))
+    par <- c(runif(length(parameters)), unique(sigma))
   } else {
-    sigma <- var_formula(X)
+    cov_par = definir_parametros(as.character(var_formula[[3]]), data = data)$parametros
+    parameters = c(parameters, cov_par)
+    par <- runif(length(parameters))
+    for (i in 1:length(cov_par)) {
+      assign(cov_par[i], par[i])
+    }
+    sigma <- parse(text = as.character(var_formula)[3]) |> eval()
     cov_sigma <- matrix(diag(sigma), ncol = length(y))
   }
-  parameters <- definir_parametros(as.character(mu_formula[[3]]), data = data)$parametros
 
-  par <- runif(length(parameters))
-  if (is.null(var_formula)){
-    par <- c(runif(length(parameters)), unique(sigma))
-  }
-
+  #return(parameters)
   model <- c()
 
 
@@ -38,11 +41,8 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal") {
     sol1 <- function() {
       return(((t(matrix(c(y - mu))) %*% cov_sigma) %*% matrix(c(y - mu))))
     }
-    det1 <- function() {
-      return(det(as.matrix(cov_sigma)))
-    }
     quadratic_part <- sapply(sol1(), \(x) ifelse(x == 0, x + 0.001, x))
-    determinant <- det1()
+    determinant <- det(as.matrix(cov_sigma))
     return(1 / 2 * sum(log(determinant)) -  sum(choose_dist(dist, quadratic_part)))
   }
 
@@ -66,17 +66,11 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal") {
   return(model_presentation)
 }
 
-X <- data.frame(x1 = runif(50), x2 = runif(50))
+dados = data.frame(X=c(405.65,498.75,567.25,618.3,681.45,405.65,498.75,567.25,618.3,681.45,681.45,681.45,681.45,681.45,681.45,681.45,681.45,681.45),Y=c(90.5,161.6,246.743,422.936,868.662,113.383,207.65,309.514,460.686,972.383,999.633,1034,1047,1072.022,1133.287,1141.883,1266.290,1169.767))
 
 
-y <- 2 / exp(5*X$x1) + log(3*X$x2)
 
-dados <- data.frame(y, X)
-
-generalreg(dados, mu_formula = y ~ beta0 / exp(beta1*x1) + log(beta2*x2), dist='normal')
-
-
-generalreg(mtcars, mu_formula = mpg ~ alfa + 1 / (beta*disp), dist='normal')
+generalreg(dados, mu_formula = Y~beta1*exp(beta2*X), dist='normal', var_formula = t ~ sigma1 * exp(sigma2*X))
 
 
 
