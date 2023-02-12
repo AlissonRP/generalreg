c961707a38d418450643560fcd8f740d2fe5e565
AlissonRP <alirpereira887@gmail.com>
2023-02-12 14:25:08 -0300

    variance estimator w

--- R/hello.R
+++ R/hello.R
@@ -10,13 +10,14 @@
 #'
  #' @examples
 #' library(generalreg)
-#' X <- data.frame(x1 = rnorm(50), x2 = rnorm(50))
-#' y <- 2 + 3 * X$x1 + 2 * X$x2
+#' X <- data.frame(x1 = rnorm(1000), x2 = rnorm(1000), x3 = rnorm(1000))
+#' e <- rnorm(1000, sd = 4)
+#' y <- 2 + 3 * X$x1 + 7 * X$x2 + e
 #' data <- data.frame(y, X)
 #' generalreg(data, mu_formula = y ~ beta0 + beta1 * x1 + beta2 * x2, dist='normal')
 #'
 #'
-#' y <- 2 / 2*X$x1
+#' y <- 2 / 2*X$x1 + e
 #' data <- data.frame(y, X)
 #' generalreg(data, mu_formula = y ~ beta0 / beta1 * x1  , dist='normal')
 #'
@@ -43,7 +44,7 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal", al
   parameters <- definir_parametros(as.character(mu_formula[[3]]), data = data)$parametros
 
   if (is.null(var_formula)) {
-    sigma <- rep(rpois(1, lambda = rpois(1, lambda = 3)), length(y))
+    sigma <- rep(0.1, length(y))
     cov_sigma <- matrix(diag(sigma), ncol = length(sigma))
     par <- c(runif(length(parameters)), unique(sigma))
   } else {
@@ -57,22 +58,28 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal", al
     cov_sigma <- matrix(diag(sigma), ncol = length(y))
   }
 
-  #return(sigma)
+
 
 
 
 
   logvero <- function(par) {
-    for (i in 1:length(parameters)) {
+    parameters = c(parameters, "sigma")
+    for (i in 1:(length(parameters) + 1)) {
       assign(parameters[i], par[i]) #initial values of parameters in formula
     }
     mu <- parse(text = as.character(mu_formula)[3]) |> eval()
-    sol1 <- function() {
-      return(((t(matrix(c(y - mu))) %*% solve(cov_sigma, tol = 1e-10000)) %*% matrix(c(y - mu))))
+    if (is.null(var_formula)) {
+      sol1 <- (matrix(t(c(y - mu))* (1 / sigma), ncol = length(y), byrow = F))  %*% matrix(c(y - mu))
+      determinant <- det(as.matrix(sigma))
+      quadratic_part <- sapply(sol1, \(x) ifelse(x == 0, x + 0.001, x))
+      return(1 / 2 * (length(mu)) * (log(determinant)) -  sum(-quadratic_part / 2))
+    } else {
+      sol1 <- ((t(matrix(c(y - mu))) %*% solve(cov_sigma, tol = 1e-10000)) %*% matrix(c(y - mu)))
+      quadratic_part <- sapply(sol1, \(x) ifelse(x == 0, x + 0.001, x))
+      determinant <- det(as.matrix(cov_sigma))
+      return(1 / 2 * sum(log(determinant)) -  sum(choose_dist(dist, quadratic_part, alpha, beta)))
     }
-    quadratic_part <- sapply(sol1(), \(x) ifelse(x == 0, x + 0.001, x))
-    determinant <- det(as.matrix(cov_sigma))
-    return(1 / 2 * sum(log(determinant)) -  sum(choose_dist(dist, quadratic_part, alpha, beta)))
   }
   coefficients <- nlminb(par, logvero,
                          gradient = NULL, hessian = NULL,
@@ -90,7 +97,7 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal", al
   }
   fit$fitted.values <- muhat()
   detach(data)
-
+  fit$initial <- par
   fit$coefficients <- coefficients
   fit$call <- call
   names(coefficients) <- parameters
@@ -100,8 +107,8 @@ generalreg <- function(data, mu_formula, var_formula = NULL, dist = "normal", al
   fit$residuals = y - fit$fitted.values
   fit$rank <- ncol(X)
   if (is.null(var_formula)){
-    fit$names <- c(parameters, 'sigma')
-    names(coefficients)[is.na(names(coefficients))] <- 'sigma'
+    fit$names <- c(parameters, 'variance')
+    names(coefficients)[is.na(names(coefficients))] <- 'variance'
 
   }
 
